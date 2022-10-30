6f9c8a552f0126709b5f948d5482f282059c5370
Nicolas Araya <naraya2018@inf.udec.cl>
2022-10-30 01:14:35 -0300

    update

--- AdjencyMatrix.cpp
+++ AdjencyMatrix.cpp
@@ -8,10 +8,6 @@ AdjencyMatrix::AdjencyMatrix(){
 AdjencyMatrix::~AdjencyMatrix(){
 }
 void AdjencyMatrix::loadFileTxt(const string path){
-
-
-
-
     this->path = path; 
     ifstream file; 
     file.open(path); 
@@ -20,40 +16,30 @@ void AdjencyMatrix::loadFileTxt(const string path){
         exit(0);
     }
 
-
     string line; 
-    file >> line;
+    getline(file, line); //num nodes
     countNodes = atoll(line.c_str());
 
     int countAux =0; 
     while(!file.eof()){
         getline(file, line);
-        //cout << line << endl;
-
         vector<uint64_t> nodes = splitString(line, " ");
-
-        matrix.push_back(make_pair(nodes[0], nodes)); //push Nodo y Nodos Adyacentes.
-
+        //cout << "i: " << countAux << " , size: " << nodes.size() << endl; 
+        if(nodes.size() > 0) matrix.push_back(make_pair(nodes[0], nodes)); //push Nodo y Nodos Adyacentes.
         if(countAux++ == 10) break;
-
     }
-
 }
 
-
-
 vector<uint64_t> AdjencyMatrix::splitString(string line, string delims){
-string::size_type bi, ei;
-   vector<uint64_t> nodes;
-   bi = line.find_first_not_of(delims);     
-   while(bi != string::npos) {
-  	ei = line.find_first_of(delims, bi);
-    	if(ei == string::npos)
-      		ei = line.length();
+    string::size_type bi, ei;
+    vector<uint64_t> nodes;
+    bi = line.find_first_not_of(delims);     
+    while(bi != string::npos) {
+  	    ei = line.find_first_of(delims, bi);
+    	if(ei == string::npos) ei = line.length();
 		string aux = line.substr(bi,ei-bi);
-    		nodes.push_back(atoi(aux.c_str()));
-    		bi = line.find_first_not_of(delims, ei);
+    	nodes.push_back(atoi(aux.c_str()));
+    	bi = line.find_first_not_of(delims, ei);
   	}                 
-  return nodes;
-
+    return nodes;
 }
