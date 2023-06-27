106f142db73e9fae8d893b55cc6e21859d9d29b5
Nicolás Araya <naraya2018@inf.udec.cl>
2023-06-27 02:06:58 -0400

    graph generator

---
+++ Utils/generator/generator.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string.h>
+#include <time.h>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+typedef struct{
+    set<unsigned> S;
+    set<pair<unsigned,unsigned>> C; 
+}Biclique;
+
+map<unsigned, set<pair<unsigned, unsigned>>>* generateBicliques(unsigned graphSize, unsigned numBicliques, unsigned averageBicliqueSize, unsigned averageWeight){
+    srand(time(NULL));
+    ofstream file; 
+    file.open("bicliques_generated.txt", std::ofstream::out | std::ofstream::app);
+
+    auto info = new map<unsigned, set<pair<unsigned, unsigned>>>();
+    
+    for(size_t i = 0; i < numBicliques; i++){
+        unsigned size = averageBicliqueSize + rand()%(unsigned)(averageBicliqueSize * 0.1);
+        size = size - rand()%(unsigned)(averageBicliqueSize * 0.1);
+
+        unsigned sizeS = (unsigned)(size*0.1) + rand()%(unsigned)(size * 0.12);
+        unsigned sizeC = (unsigned)(size/sizeS);
+        size = sizeS * sizeC;
+
+        Biclique temp; 
+
+        while(temp.S.size() < sizeS){
+            int tempSize = temp.S.size();
+            while(temp.S.size() == tempSize){
+                unsigned id = rand()%graphSize;
+                temp.S.insert(id);
+            }
+        }
+
+        while(temp.C.size() < sizeC){
+            int tempSize = temp.C.size();
+            while(temp.C.size() == tempSize){
+                unsigned ady = rand()%graphSize;
+                unsigned weight = averageWeight + rand()%(unsigned)(averageWeight * 0.5);
+                weight = weight - rand()%(unsigned)(averageWeight * 0.5);
+                temp.C.insert(make_pair(ady,weight));
+            }
+        }
+
+        for(auto id : temp.S){
+           if(info->count(id) > 0){
+                for(auto it : temp.C){
+                    info->at(id).insert(it);
+                }
+            }
+            else{
+                info->insert(make_pair(id, temp.C));
+            }
+            file << id;
+            if(id != *temp.S.end()){
+                file << " ";
+            }
+        }
+        file << endl;
+        for(auto ady : temp.C){
+            file << "(" << ady.first << "," << ady.second << ")";
+            if(ady != *temp.C.end()){
+                file << " ";
+            }
+        }
+        file << endl;
+
+    }   
+
+    file.close(); 
+    return info;
+
+}
+
+void generateGraph(unsigned graphNodes, unsigned averageEdges, unsigned averageWeight, map<unsigned, set<pair<unsigned, unsigned>>>* info){
+    ofstream file; 
+    file.open("graph_generated.txt", std::ofstream::out | std::ofstream::app);
+
+
+
+    for(size_t i = 0; i < graphNodes; i++){
+        unsigned size = averageEdges + rand()%(unsigned)(averageEdges * 0.1);
+        size = size - rand()%(unsigned)(averageEdges * 0.1);
+        set<pair<unsigned, unsigned>>* adyacents; 
+        auto f = info->find(i);
+        if(f != info->end()){
+            adyacents = &(*f).second;
+        }
+        else {
+            adyacents = new set<pair<unsigned, unsigned>>();
+        }
+
+        while(adyacents->size() < size){
+            int tempSize = adyacents->size();
+            while(adyacents->size() == tempSize){
+                unsigned ady = rand()%graphNodes;
+                unsigned weight = averageWeight + rand()%(unsigned)(averageWeight * 0.5);
+                weight = weight - rand()%(unsigned)(averageWeight * 0.5);
+                adyacents->insert(make_pair(ady,weight));
+            }
+
+        }
+        for(auto ady : *adyacents){
+            file << i << " " << ady.first << " " << ady.second << endl;
+        }
+    }
+    file.close();
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+    if(argc < 6){
+        return 0;
+    } 
+    unsigned graphNodes = atoll(argv[1]);
+    unsigned averageEdges = atoll(argv[2]);
+    unsigned numBicliques = atoll(argv[3]);
+    unsigned averageBicliqueSize = atoll(argv[4]);
+    unsigned averageWeight = atoll(argv[5]);
+
+    if(averageWeight > averageEdges){
+        averageWeight = averageEdges * 0.75;
+    }
+
+    auto info = generateBicliques(  graphNodes, 
+                                    numBicliques, 
+                                    averageBicliqueSize, 
+                                    averageWeight   );
+
+    generateGraph(graphNodes, averageEdges, averageWeight, info);
+
+    return 0;
+}
