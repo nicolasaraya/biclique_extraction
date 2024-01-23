a3fdaddda679e9c343b067fdfc95bd4efd6636b6
nicolasaraya <naraya2018@udec.cl>
2024-01-23 16:46:30 -0300

    update

--- Utils/generator/compila.sh
+++ Utils/generator/compila.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ generator.cpp ../Utils.hpp ../Utils.cpp ../../Graph/Node.hpp ../../Graph/Node.cpp ../../Graph/GraphWeighted.hpp ../../Graph/GraphWeighted.cpp -O0 -g -o generator.out
+g++ generator.cpp ../Utils.hpp ../Utils.cpp ../../Graph/Node.hpp ../../Graph/Node.cpp ../../Graph/GraphWeighted.hpp ../../Graph/GraphWeighted.cpp -O3 -o generator.out
 
